ebef43fabc0a04ff5f32e2c4ffe01631152de8e9
Henry Huang <henry2423@gmail.com>
2023-08-20 16:35:04 -0700

    Initial Commit

---
+++ IIIFVisionAR/IIIFVisionARApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIIFVisionARApp.swift
+//  IIIFVisionAR
+//
+//  Created by Henry Huang on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct IIIFVisionARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
