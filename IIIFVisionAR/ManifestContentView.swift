abff8e0643a2cb6df59bc79f1a71f286eaf91984
Henry Huang <henry2423@gmail.com>
2023-08-21 21:39:18 -0700

    Add the open/close toggle

--- IIIFVisionAR/ManifestContentView.swift
+++ IIIFVisionAR/ManifestContentView.swift
@@ -9,51 +9,32 @@ import SwiftUI
 
 struct ManifestContentView: View {
     @Environment(\.openImmersiveSpace) private var openImmersiveSpace
-    @Environment(\.dismissWindow) private var dismissWindow
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    @State private var isImmersiveSpaceOpened = false
 
     var body: some View {
         NavigationStack {
             VStack {
-                Button("Present Solar System In New Immersive Space") {
-                    Task {
-                        await openImmersiveSpace(id: "SingleImage")
+                if isImmersiveSpaceOpened {
+                    Button("Close the IIIF") {
+                        Task {
+                            await dismissImmersiveSpace()
+                            isImmersiveSpaceOpened = false
+                        }
+                    }
+                } else {
+                    Button("Present IIIF in Immersive Space") {
+                        Task {
+                            await openImmersiveSpace(id: "SingleImage")
+                            isImmersiveSpaceOpened = true
+                        }
                     }
                 }
-//                NavigationLink {
-//    //                ARContentView(avgWidth: viewModel.averageImageWidth,
-//    //                              avgLength: viewModel.averageImageLength,
-//    //                              image_urls: viewModel.imageEntities.compactMap { $0.localImageURL },
-//    //                              currentIndex: selectedItem,
-//    //                              title: viewModel.values[0],
-//    //                              widthratio: viewModel.widthratio,
-//    //                              heightratio: viewModel.heightratio)
-//    //                .onAppear {
-//    //                    AnalyticsEventService.shared.logEvent(.init(name: .itemARViewed, attributes: [
-//    //                        "manifest_url": viewModel.sourceURLString
-//    //                    ]))
-//    //                }
-//                    SingleImageRealityView()
-//                } label: {
-//                    ZStack(){
-//                        Color.init(.systemBlue)
-//                            .cornerRadius(10)
-//                        HStack {
-//                            Image(systemName: "arkit")
-//                                .foregroundColor(.white)
-//                            Text("View in Your Space")
-//                                .foregroundColor(.white)
-//                                .font(.system(size: 18, weight: .medium, design: .default))
-//                        }
-//                    }
-//                    .frame(height: 50.0)
-//                    .padding(EdgeInsets(top: 10, leading: 30, bottom: 10, trailing: 30))
-//                }
             }
-            .padding()
         }
     }
 }
 
-//#Preview {
-//    ManifestContentView()
-//}
+#Preview {
+    ManifestContentView()
+}
