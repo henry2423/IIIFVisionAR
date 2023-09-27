59d56b9e5578ef5446963a2b26b9dae3faff75a4
Henry Huang <henry2423@gmail.com>
2023-09-26 17:30:41 -0700

    Add volumetric button

--- IIIFVisionAR/ManifestContentView.swift
+++ IIIFVisionAR/ManifestContentView.swift
@@ -11,36 +11,78 @@ struct ManifestContentView: View {
     @Environment(\.openImmersiveSpace) private var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
     @State private var isImmersiveSpaceOpened = false
+    @State private var isShowingVolumetric = false
 
     var body: some View {
         NavigationStack {
             VStack {
-                if isImmersiveSpaceOpened {
-                    Button("Close the IIIF") {
-                        Task {
-                            await dismissImmersiveSpace()
-                            isImmersiveSpaceOpened = false
-                        }
-                    }
-                } else {
-                    Button("Present Single IIIF in Immersive Space") {
-                        Task {
-                            await openImmersiveSpace(id: "SingleImage")
-                            isImmersiveSpaceOpened = true
-                        }
-                    }
+                SpaceToggle(
+                    title: "Present Single IIIF in Immersive Space",
+                    id: "SingleImage",
+                    isShowing: $isImmersiveSpaceOpened)
 
-                    Button("Present Compound IIIF in Immersive Space") {
-                        Task {
-                            await openImmersiveSpace(id: "CompoundImage")
-                            isImmersiveSpaceOpened = true
-                        }
-                    }
-                }
+                SpaceToggle(
+                    title: "Present Compound IIIF in Immersive Space",
+                    id: "CompoundImage",
+                    isShowing: $isImmersiveSpaceOpened)
+
+                WindowToggle(
+                    title: "Present Compound IIIF in Volumetric Space",
+                    id: "CompoundImageVolume",
+                    isShowing: $isShowingVolumetric)
             }
         }
     }
 }
+
+/// A toggle that activates or deactivates a window with
+/// the specified identifier.
+private struct WindowToggle: View {
+    var title: String
+    var id: String
+    @Binding var isShowing: Bool
+
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+
+    var body: some View {
+        Toggle(title, isOn: $isShowing)
+            .onChange(of: isShowing) { wasShowing, isShowing in
+                if isShowing {
+                    openWindow(id: id)
+                } else {
+                    dismissWindow(id: id)
+                }
+            }
+            .toggleStyle(.button)
+    }
+}
+
+/// A toggle that activates or deactivates the immersive space with
+/// the specified identifier.
+private struct SpaceToggle: View {
+    var title: String
+    var id: String
+    @Binding var isShowing: Bool
+
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+
+    var body: some View {
+        Toggle(title, isOn: $isShowing)
+            .onChange(of: isShowing) { wasShowing, isShowing in
+                Task {
+                    if isShowing {
+                        await openImmersiveSpace(id: id)
+                    } else {
+                        await dismissImmersiveSpace()
+                    }
+                }
+            }
+            .toggleStyle(.button)
+    }
+}
+
 
 #Preview {
     ManifestContentView()
